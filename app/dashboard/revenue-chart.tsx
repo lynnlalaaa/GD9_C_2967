4f49a57d07c8bae35bec107d65ddb4227578f62a
lynnlalaaa <stellaolivia909@gmail.com>
2026-05-09 15:42:10 +0700

    fix revenue-chart error

--- app/dashboard/revenue-chart.tsx
+++ app/dashboard/revenue-chart.tsx
@@ -6,15 +6,15 @@ import { fetchRevenue } from '@/app/lib/data';
 
 export default async function RevenueChart() { // Make component async, remove the props
   const revenue = await fetchRevenue(); // Fetch data inside the component
- 
+
   const chartHeight = 350;
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
- 
+
   if (!revenue || revenue.length === 0) {
     return <p className="mt-4 text-gray-400">No data available.</p>;
   }
- 
+
   return (
-    // ...
+    <div>Revenue Chart</div>
   );
 }
